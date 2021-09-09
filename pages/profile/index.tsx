df2151a25802f67367e739f4455bc59c3418dd90
ElTioGonza <gonz230300@gmail.com>
2021-09-09 10:11:40 -0300

    Display more user data on /profile PAGE

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,27 +1,34 @@
-import { Square } from "@chakra-ui/layout";
+import { Editable, EditableInput, EditablePreview } from "@chakra-ui/editable";
+import { Container, Square } from "@chakra-ui/layout";
 import { Spinner } from "@chakra-ui/spinner";
 import { NextPage } from "next";
 import { useSession } from "next-auth/client";
 import { useEffect } from "react";
 import { User } from "../../components/Card";
+import EditableDescription from "../../components/EditableDescription";
 import Heading from "../../components/Heading";
+import ImagesSlider from "../../components/ImagesSlider";
 import Loading from "../../components/Loading";
+import { fontSize } from "../../lib/styles";
 
 const Profile: NextPage = () => {
   const [session] = useSession()
-  const user = session?.user as User | undefined
+  const user = session?.user as User & { images: string } | undefined
   const isLoggedOut = session === null
   const isLoggedIn = session !== undefined && session !== null && user
   useEffect(() => {
     if (isLoggedOut) window.location.replace("/login")
+    console.log(user)
   }, [session])
   return (
     <>
       {
         isLoggedIn ?
-          <>
-            <Heading>{user.name}</Heading>
-          </>
+          <Container>
+            <Heading py="1em">{user.name}</Heading>
+            <ImagesSlider images={JSON.parse(user.images)} />
+            <EditableDescription defaultValue={user.description} />
+          </Container>
           :
           <Loading />
       }
