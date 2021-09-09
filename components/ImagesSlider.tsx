b998bdd363ccc79e3340ea4cfe03747a3c4fec1c
ElTioGonza <gonz230300@gmail.com>
2021-09-09 09:49:18 -0300

    Modified ImagesSlider to show a black images placeholder when passed an empty array

--- components/ImagesSlider.tsx
+++ components/ImagesSlider.tsx
@@ -14,21 +14,27 @@ function ImagesSlider({ images, ...props }: Props): ReactElement {
   const [current, setCurrent] = useState(0)
   return (
     <Box pos="relative" overflow="hidden" {...props}>
-      <Box display="flex" transitionDuration="400ms" transform={`translateX(-${current * 100 / images.length}%)`} w={`${images.length * 100}%`}>
-        {
-          images.map((image, id) => (
-            <Box key={id} w={`${100 / images.length}%`}>
-              <Image w="100%" minH="60vh" maxH="35rem" alt="Imagen del usuario" fit="contain" bg="black" src={image} fallback={<Box w="100%" minH="60vh" maxH="35rem" bg="black" />} />
-            </Box>
-          ))
-        }
-      </Box>
-      <IconButton opacity={first() ? "0.25" : "1"} colorScheme="whiteAlpha" rounded="full" aria-label="Ir a la imagen anterior" pos="absolute" top="50%" transform="translateY(-50%)" left="0.5rem" onClick={left}>
-        <FontAwesomeIcon icon={faArrowLeft} />
-      </IconButton>
-      <IconButton opacity={last() ? "0.25" : "1"} colorScheme="whiteAlpha" rounded="full" aria-label="Ir a la siguiente imagen" pos="absolute" top="50%" transform="translateY(-50%)" right="0.5rem" onClick={right}>
-        <FontAwesomeIcon icon={faArrowRight} />
-      </IconButton>
+      {images.length !== 0 ?
+        <>
+          <Box display="flex" transitionDuration="400ms" transform={`translateX(-${current * 100 / images.length}%)`} w={`${images.length * 100}%`}>
+            {
+              images.map((image, id) => (
+                <Box key={id} w={`${100 / images.length}%`}>
+                  <Image w="100%" minH="60vh" maxH="35rem" alt="Imagen del usuario" fit="contain" bg="black" src={image} fallback={<Box w="100%" minH="60vh" maxH="35rem" bg="black" />} />
+                </Box>
+              ))
+            }
+          </Box>
+          <IconButton opacity={first() ? "0.25" : "1"} colorScheme="whiteAlpha" rounded="full" aria-label="Ir a la imagen anterior" pos="absolute" top="50%" transform="translateY(-50%)" left="0.5rem" onClick={left}>
+            <FontAwesomeIcon icon={faArrowLeft} />
+          </IconButton>
+          <IconButton opacity={last() ? "0.25" : "1"} colorScheme="whiteAlpha" rounded="full" aria-label="Ir a la siguiente imagen" pos="absolute" top="50%" transform="translateY(-50%)" right="0.5rem" onClick={right}>
+            <FontAwesomeIcon icon={faArrowRight} />
+          </IconButton>
+        </>
+        :
+        <Box w="100%" bg="black" minH="60vh" maxH="35rem" />
+      }
     </Box>
   )
 
