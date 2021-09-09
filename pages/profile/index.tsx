0242d364f7c195fd5e98fa7c459eb55201e03b9c
ElTioGonza <gonz230300@gmail.com>
2021-09-09 10:20:33 -0300

    Added Gender and Age data to /profile PAGE

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,6 +1,8 @@
 import { Editable, EditableInput, EditablePreview } from "@chakra-ui/editable";
-import { Container, Square } from "@chakra-ui/layout";
+import { Badge, Container, HStack, Square } from "@chakra-ui/layout";
 import { Spinner } from "@chakra-ui/spinner";
+import { faMars, faVenus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { NextPage } from "next";
 import { useSession } from "next-auth/client";
 import { useEffect } from "react";
@@ -14,6 +16,8 @@ import { fontSize } from "../../lib/styles";
 const Profile: NextPage = () => {
   const [session] = useSession()
   const user = session?.user as User & { images: string } | undefined
+  const colorScheme = user?.gender === "woman" ? "pink" : "blue"
+  const genderIcon = user?.gender === "woman" ? faVenus : faMars
   const isLoggedOut = session === null
   const isLoggedIn = session !== undefined && session !== null && user
   useEffect(() => {
@@ -27,6 +31,12 @@ const Profile: NextPage = () => {
           <Container>
             <Heading py="1em">{user.name}</Heading>
             <ImagesSlider images={JSON.parse(user.images)} />
+            <HStack spacing="2" py="4">
+              <Badge fontSize={fontSize.paragraph} colorScheme={colorScheme}>{user.age}</Badge>
+              <Badge px="0.5em" fontSize={fontSize.paragraph} colorScheme={colorScheme}>
+                <FontAwesomeIcon icon={genderIcon} />
+              </Badge>
+            </HStack>
             <EditableDescription defaultValue={user.description} />
           </Container>
           :
