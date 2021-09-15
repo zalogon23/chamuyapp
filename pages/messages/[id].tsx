285b8f9348fe0ac530c0401ae3ce18206fef6836
ElTioGonza <gonz230300@gmail.com>
2021-09-15 16:40:27 -0300

    Added /messages/:id PAGE. Redirects if you arent logged in, or there are no messages.

---
+++ pages/messages/[id].tsx
@@ -0,0 +1,43 @@
+import { NextPage } from "next";
+import Router from "next/router";
+import { useContext, useEffect, useState } from "react";
+import Heading from "../../components/Heading";
+import { messagesContext } from "../../context/messages";
+import { userContext } from "../../context/user";
+
+interface Message {
+  content: string,
+  id: number,
+  createdAt: Date,
+  receiverID: number,
+  senderID: number
+}
+
+const MessagesID: NextPage = () => {
+  const { isLoggedOut } = useContext(userContext)
+  const { matchesMessages, matchesNoMessages } = useContext(messagesContext)
+  const [messages, setMessages] = useState([] as Message[])
+  useEffect(() => {
+    const pathname = window?.location.pathname
+    const id = Number(pathname.slice(pathname.indexOf("/", 1) + 1))
+    if (id) {
+      if (matchesMessages.length) {
+        const currentMessages = JSON.parse(matchesMessages.find(match => match.id === id)?.content || "[]") as Message[]
+        if (!Boolean(currentMessages?.length)) Router.replace("/messages")
+        setMessages(currentMessages)
+      }
+    }
+  }, [matchesMessages])
+  useEffect(() => {
+    if (isLoggedOut) {
+      Router.replace("/login")
+    }
+  }, [isLoggedOut])
+  return (
+    <>
+      <Heading>Hello</Heading>
+    </>
+  )
+}
+
+export default MessagesID
