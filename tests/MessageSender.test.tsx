c8397b462ff12e23721e994824fd7d38e05bd7e5
ElTioGonza <gonz230300@gmail.com>
2021-09-17 16:47:58 -0300

    Added test to MessageSender COMPONENT (inside of /messages/:id PAGE).

---
+++ tests/MessageSender.test.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+import { render, screen } from "@testing-library/react"
+import { MessageSender } from "../pages/messages/[id]"
+
+describe("Message Sender COMPONENT", () => {
+  const name = "anotheruser"
+  let input: HTMLElement, button: HTMLElement
+  beforeAll(() => {
+    render(<MessageSender name={name} />)
+    input = screen.getByRole("textbox")
+    button = screen.getByRole("button")
+  })
+  it("should render an input and button", () => {
+    expect(input).toBeInTheDocument()
+    expect(button).toBeInTheDocument()
+  })
+})
