b796ec4d68dc0c3f647956cfaaceec03eee3ad5c
ElTioGonza <gonz230300@gmail.com>
2021-09-22 19:20:18 -0300

    Modified NoUsers COMPONENT to show one message when the user is logged in and another when is not.

--- components/NoUsers.tsx
+++ components/NoUsers.tsx
@@ -1,19 +1,32 @@
 import { Square, Button } from '@chakra-ui/react'
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useContext } from 'react'
 import Heading from './Heading'
 import Text from './Text'
 import Link from "next/link"
+import { userContext } from '../context/user'
 
 interface Props {
 
 }
 
 function NoUsers({ }: Props): ReactElement {
+  const { isLoggedIn, isLoggedOut } = useContext(userContext)
   return (
     <Square flexDir="column" minH="90vh" p="4">
-      <Heading>No hay mas usuarios que mostrar</Heading>
-      <Text pt="4" pb="8">Puedes cambiar tus preferencias para seguir dando "me gusta"!</Text>
-      <Link href="/profile" passHref><Button aria-label="Ir a mi perfil">Preferencias</Button></Link>
+      {isLoggedIn &&
+        <>
+          <Heading>No hay mas usuarios que mostrar</Heading>
+          <Text pt="4" pb="8">Puedes cambiar tus preferencias para seguir dando "me gusta"!</Text>
+          <Link href="/profile" passHref><Button aria-label="Ir a mi perfil">Preferencias</Button></Link>
+        </>
+      }
+      {isLoggedOut &&
+        <>
+          <Heading>No estas logueado</Heading>
+          <Text pt="4" pb="8">Logueate para poder ver usuarios</Text>
+          <Link href="/login" passHref><Button aria-label="Ir a loguearme">Loguearme</Button></Link>
+        </>
+      }
     </Square>
   )
 }
