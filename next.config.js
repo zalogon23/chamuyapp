d0c2bf6586bc3f2290b502e67aea314e2ded9c7f
ElTioGonza <gonz230300@gmail.com>
2021-09-22 20:34:49 -0300

    Modified apolloClient to use only process.env.GRAPHQL_URL .

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ module.exports = {
   reactStrictMode: true,
   env: {
     CLIENT_ID_GITHUB: "Iv1.07959572315df8ac",
-    CLIENT_SECRET_GITHUB: "3f629d49083fd23404a628ad34648bc33a7ac87e"
+    CLIENT_SECRET_GITHUB: "3f629d49083fd23404a628ad34648bc33a7ac87e",
+    GRAPHQL_URL: "http://192.168.0.2:8000/graphql"
   }
 }
