d567429472950bfdd3e2bb5e5bc331b78da37fc6
ElTioGonza <gonz230300@gmail.com>
2021-09-22 20:43:34 -0300

    Setted ENV variable to actual graphql server.

--- next.config.js
+++ next.config.js
@@ -4,6 +4,6 @@ module.exports = {
   env: {
     CLIENT_ID_GITHUB: "Iv1.07959572315df8ac",
     CLIENT_SECRET_GITHUB: "3f629d49083fd23404a628ad34648bc33a7ac87e",
-    GRAPHQL_URL: "http://192.168.0.2:8000/graphql"
+    GRAPHQL_URL: "https://chamuyappadmin.herokuapp.com/graphql"
   }
 }
