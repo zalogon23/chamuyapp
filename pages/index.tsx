3fb10f8132a2d534febe2ecf27763bb4aeab3a7d
ElTioGonza <gonz230300@gmail.com>
2021-09-23 18:57:15 -0300

    Updated / PAGE head tag.

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import MatchModal from "../components/MatchModal"
 import CardsSlider from '../components/CardsSlider'
 import { showingUsersContext } from "../context/showingUsers"
 import { messagesContext } from '../context/messages'
+import SEOHead from '../components/SEOHead'
 
 const Home: NextPage = () => {
   const { match, setMatch } = useContext(showingUsersContext)
@@ -16,6 +17,11 @@ const Home: NextPage = () => {
   }, [match])
   return (
     <>
+      <SEOHead
+        title="Descubre gente nueva"
+        keywords="ligar chamuyar romance choque y fuga pareja sexo casual amor conocer hobbies gustos"
+        description="Encuentra gente nueva con quien hablar, salir al parque a dar una vuelta y pasar buenos momentos."
+      />
       <MatchModal
         open={Boolean(match)}
         name={match ? match.name : undefined}
