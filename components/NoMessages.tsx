be0c86202508621a9a7b375b1c9fe559123c3d2c
ElTioGonza <gonz230300@gmail.com>
2021-09-23 17:28:17 -0300

    Modified NoMessages COMPONENT to show a different state when logged out.

--- components/NoMessages.tsx
+++ components/NoMessages.tsx
@@ -1,20 +1,32 @@
 import { Square } from '@chakra-ui/layout'
 import { Button } from '@chakra-ui/button'
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useContext } from 'react'
 import Heading from './Heading'
 import Text from './Text'
 import Link from "next/link"
+import { userContext } from '../context/user'
 
 interface Props {
 
 }
 
 function NoMessages({ }: Props): ReactElement {
+  const { isLoggedIn, isLoggedOut } = useContext(userContext)
   return (
     <Square flexDir="column" minH="90vh" p="4">
-      <Heading textAlign="center">No hay mensajes que mostrar</Heading>
-      <Text pt="4" pb="8">Para conseguir un match puedes seguir dando me gusta!</Text>
-      <Link href="/" passHref><Button aria-label="Ir a mi inicio">Dar me gusta</Button></Link>
+      {isLoggedIn &&
+        <>
+          <Heading textAlign="center">No hay mensajes que mostrar</Heading>
+          <Text pt="4" pb="8">Para conseguir un match puedes seguir dando me gusta!</Text>
+          <Link href="/" passHref><Button aria-label="Ir a mi inicio">Dar me gusta</Button></Link>
+        </>
+      }
+      {isLoggedOut &&
+        <>
+          <Heading textAlign="center">No estas logueado</Heading>
+          <Text pt="4" pb="8">Entra en tu cuenta para ver tus mensajes</Text>
+        </>
+      }
     </Square>
   )
 }
