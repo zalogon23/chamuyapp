3e877e73bf0985ffc35bdd3f98cb8fbf2764eb37
ElTioGonza <gonz230300@gmail.com>
2021-09-23 19:07:38 -0300

    Updated /register PAGE head tag.

--- pages/register/index.tsx
+++ pages/register/index.tsx
@@ -9,6 +9,7 @@ import Router from "next/router";
 import { useContext, useEffect } from "react";
 import Heading from "../../components/Heading";
 import Loading from "../../components/Loading";
+import SEOHead from "../../components/SEOHead";
 import { userContext } from "../../context/user";
 import client from "../../lib/apolloClient";
 import queries from "../../lib/queries";
@@ -27,74 +28,81 @@ const Login: NextPage = () => {
     if (isLoggedIn) Router.replace("/profile")
   }, [isLoggedIn])
   return (
-    <Container>
-      {
-        isLoggedOut ?
-          <Formik
-            initialValues={{
-              name: "",
-              email: "",
-              password: "",
-              gender: "woman",
-              age: 18
-            }}
-            validate={values => validate(values)}
-            onSubmit={async (values, { setSubmitting, resetForm }) => {
-              const result = await client.mutate({
-                mutation: queries.register, variables: {
-                  createUserVariables: { ...values }
-                }
-              })
-              resetForm()
-              setSubmitting(false)
-              const created = !!result?.data?.createUser?.id || false
-              if (created) window.location.replace("/login")
-            }}
-          >
-            {({
-              values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting
-            }) => (
-              <Form onSubmit={handleSubmit}>
-                <Stack px="4" pb="10" spacing="3">
-                  <Heading pt="1.5em" pb="1em">Registrate</Heading>
-                  <FormControl>
-                    <FormLabel htmlFor="name" id="nameLabel">Nombre:</FormLabel>
-                    <Input id="name" borderColor={errors.name && touched.name ? "red.500" : ""} value={values.name} onBlur={handleBlur} onChange={handleChange} name="name" type="text" />
-                    <FormHelperText id="nameHelp">{touched.name && errors.name}</FormHelperText>
-                  </FormControl>
-                  <FormControl>
-                    <FormLabel htmlFor="email" id="emailLabel">Email:</FormLabel>
-                    <Input id="email" borderColor={errors.email && touched.email ? "red.500" : ""} value={values.email} onBlur={handleBlur} onChange={handleChange} name="email" type="email" />
-                    <FormHelperText id="emailHelp">{touched.email && errors.email}</FormHelperText>
-                  </FormControl>
-                  <FormControl>
-                    <FormLabel htmlFor="password" id="passwordLabel">Contraseña:</FormLabel>
-                    <Input id="password" borderColor={errors.password && touched.password ? "red.500" : ""} value={values.password} onBlur={handleBlur} onChange={handleChange} name="password" type="password" />
-                    <FormHelperText id="passwordHelp">{touched.password && errors.password}</FormHelperText>
-                  </FormControl>
-                  <FormControl>
-                    <FormLabel htmlFor="age" id="ageLabel">Edad:</FormLabel>
-                    <Input id="age" borderColor={errors.age && touched.age ? "red.500" : ""} value={values.age} onBlur={handleBlur} onChange={handleChange} name="age" type="number" />
-                    <FormHelperText id="ageHelp">{touched.age && errors.age}</FormHelperText>
-                  </FormControl>
-                  <FormControl pb="6" as="fieldset">
-                    <FormLabel htmlFor="genderGroup" id="gender" as="legend">Soy:</FormLabel>
-                    <RadioGroup id="genderGroup" name="gender" value={values.gender}>
-                      <HStack spacing="4">
-                        <Radio id="radioWoman" onChange={handleChange} name="gender" value="woman">Mujer</Radio>
-                        <Radio id="radioMan" onChange={handleChange} name="gender" value="man">Hombre</Radio>
-                      </HStack>
-                    </RadioGroup>
-                    <FormHelperText id="genderHelp">Elegí el que te represente mas</FormHelperText>
-                  </FormControl>
-                  <Button isLoading={isSubmitting} type="submit">Dale!</Button>
-                </Stack>
-              </Form>
-            )}
-          </Formik> :
-          <Loading />
-      }
-    </Container>
+    <>
+      <SEOHead
+        title="Registrate para que empieces a conocer gente nueva"
+        keywords="ligar registarte registro chamuyar romance choque y fuga pareja sexo casual amor conocer hobbies gustos"
+        description="El primer paso para que puedas descubrir quienes desean conocerte. Registrate y podrás empezar a elegir."
+      />
+      <Container>
+        {
+          isLoggedOut ?
+            <Formik
+              initialValues={{
+                name: "",
+                email: "",
+                password: "",
+                gender: "woman",
+                age: 18
+              }}
+              validate={values => validate(values)}
+              onSubmit={async (values, { setSubmitting, resetForm }) => {
+                const result = await client.mutate({
+                  mutation: queries.register, variables: {
+                    createUserVariables: { ...values }
+                  }
+                })
+                resetForm()
+                setSubmitting(false)
+                const created = !!result?.data?.createUser?.id || false
+                if (created) window.location.replace("/login")
+              }}
+            >
+              {({
+                values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting
+              }) => (
+                <Form onSubmit={handleSubmit}>
+                  <Stack px="4" pb="10" spacing="3">
+                    <Heading pt="1.5em" pb="1em">Registrate</Heading>
+                    <FormControl>
+                      <FormLabel htmlFor="name" id="nameLabel">Nombre:</FormLabel>
+                      <Input id="name" borderColor={errors.name && touched.name ? "red.500" : ""} value={values.name} onBlur={handleBlur} onChange={handleChange} name="name" type="text" />
+                      <FormHelperText id="nameHelp">{touched.name && errors.name}</FormHelperText>
+                    </FormControl>
+                    <FormControl>
+                      <FormLabel htmlFor="email" id="emailLabel">Email:</FormLabel>
+                      <Input id="email" borderColor={errors.email && touched.email ? "red.500" : ""} value={values.email} onBlur={handleBlur} onChange={handleChange} name="email" type="email" />
+                      <FormHelperText id="emailHelp">{touched.email && errors.email}</FormHelperText>
+                    </FormControl>
+                    <FormControl>
+                      <FormLabel htmlFor="password" id="passwordLabel">Contraseña:</FormLabel>
+                      <Input id="password" borderColor={errors.password && touched.password ? "red.500" : ""} value={values.password} onBlur={handleBlur} onChange={handleChange} name="password" type="password" />
+                      <FormHelperText id="passwordHelp">{touched.password && errors.password}</FormHelperText>
+                    </FormControl>
+                    <FormControl>
+                      <FormLabel htmlFor="age" id="ageLabel">Edad:</FormLabel>
+                      <Input id="age" borderColor={errors.age && touched.age ? "red.500" : ""} value={values.age} onBlur={handleBlur} onChange={handleChange} name="age" type="number" />
+                      <FormHelperText id="ageHelp">{touched.age && errors.age}</FormHelperText>
+                    </FormControl>
+                    <FormControl pb="6" as="fieldset">
+                      <FormLabel htmlFor="genderGroup" id="gender" as="legend">Soy:</FormLabel>
+                      <RadioGroup id="genderGroup" name="gender" value={values.gender}>
+                        <HStack spacing="4">
+                          <Radio id="radioWoman" onChange={handleChange} name="gender" value="woman">Mujer</Radio>
+                          <Radio id="radioMan" onChange={handleChange} name="gender" value="man">Hombre</Radio>
+                        </HStack>
+                      </RadioGroup>
+                      <FormHelperText id="genderHelp">Elegí el que te represente mas</FormHelperText>
+                    </FormControl>
+                    <Button isLoading={isSubmitting} type="submit">Dale!</Button>
+                  </Stack>
+                </Form>
+              )}
+            </Formik> :
+            <Loading />
+        }
+      </Container>
+    </>
   )
   function validate(values: Values): Values {
     const errors = {} as Values;
