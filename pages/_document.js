7c3af61469e9252d972d71dbafe66e67bb2b3108
ElTioGonza <gonz230300@gmail.com>
2021-09-23 20:01:50 -0300

    FIXED styling and favicon are WORKING. (had to modified _document.js).

---
+++ pages/_document.js
@@ -0,0 +1,23 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+          <link href="https://fonts.googleapis.com/css2?family=Satisfy&display=swap" rel="stylesheet" />
+          <link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32" />
+          <link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
