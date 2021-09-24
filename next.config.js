f75fb9c4a399658a2238371211a15a28b49bdeb1
ElTioGonza <gonz230300@gmail.com>
2021-09-24 15:33:55 -0300

    Modified next.config.js to redirect users from different routes to the right ones.

--- next.config.js
+++ next.config.js
@@ -5,5 +5,29 @@ module.exports = {
     CLIENT_ID_GITHUB: "Iv1.07959572315df8ac",
     CLIENT_SECRET_GITHUB: "3f629d49083fd23404a628ad34648bc33a7ac87e",
     GRAPHQL_URL: "https://chamuyappadmin.herokuapp.com/graphql"
+  },
+  async redirects() {
+    return [
+      {
+        source: "/users([^\/]*)",
+        destination: "/",
+        permanent: true
+      },
+      {
+        source: "/perfil([^\/]*)",
+        destination: "/profile",
+        permanent: true
+      },
+      {
+        source: "/mensajes([^\/]*)",
+        destination: "/messages",
+        permanent: true
+      },
+      {
+        source: "/chat([^\/]*)",
+        destination: "/messages",
+        permanent: true
+      },
+    ]
   }
 }
