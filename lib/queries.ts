62a14918c830ef78dbb340819a14a0fbc937722d
ElTioGonza <gonz230300@gmail.com>
2021-09-24 15:35:37 -0300

    Stopped asking for unnecesary fields on getUserByID QUERY.

--- lib/queries.ts
+++ lib/queries.ts
@@ -25,7 +25,7 @@ const queries = {
   getUserByID: gql`
   query Query($getUserByIdId: Float!) {
     getUserByID(id: $getUserByIdId) {
-      id, name, gender, age, description, images, votedUsers, likedUsers, matchedUsers,genderPreference
+      id, name, gender, age, description, images, genderPreference
     }
   }
   `,
