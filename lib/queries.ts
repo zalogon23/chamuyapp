d04f63558dac88b8441f2e8672a1042e68f05b93
ElTioGonza <gonz230300@gmail.com>
2021-09-24 16:05:08 -0300

    Modified getMatches QUERY to ask for 'seen' FIELD.

--- lib/queries.ts
+++ lib/queries.ts
@@ -54,7 +54,7 @@ const queries = {
   getMatches: gql`
   query Query($getMatchesUserId: Float!) {
     getMatches(userID: $getMatchesUserId){
-      name, images, id, content, anotherID
+      name, images, id, content, anotherID, seen
     }
   }  
   `,
