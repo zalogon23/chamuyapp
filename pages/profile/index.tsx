7c20654c61cef5135964e8016e3f992646097f84
ElTioGonza <gonz230300@gmail.com>
2021-09-28 12:23:17 -0300

    Specifying that the /profile distance measure is in kilometers.

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -152,6 +152,7 @@ const Profile: NextPage = () => {
                   <EditablePreview />
                   <EditableInput textAlign="center" type="number" />
                 </Editable>
+                <Text>KM</Text>
               </HStack>
               <Tooltip label="Actualizar geolocalizacion">
                 <IconButton onClick={updateGeolocation} w="100%" colorScheme={colorScheme} aria-label="Actualizar geolocalizacion">
