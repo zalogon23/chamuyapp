56dce9806fbfd2fb07c6b0c8665500a5939c7ee3
ElTioGonza <gonz230300@gmail.com>
2021-09-29 14:48:54 -0300

    Modified Header COMPONENT. Now if your location is the default one (1000, 1000) then the Header will the trigger a geolocation request. And that will set your actual geolocation for starting.

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Link from "next/link"
 import React, { ReactElement, useContext, useEffect, useState } from 'react'
 import { messagesContext } from '../context/messages'
+import { showingUsersContext } from '../context/showingUsers'
 import { userContext } from '../context/user'
+import client from '../lib/apolloClient'
+import queries from '../lib/queries'
 import { fontSize } from '../lib/styles'
 import Heading from './Heading'
 import HNavBar from './HNavBar'
@@ -49,7 +52,8 @@ const links: Link[] = [
 
 function Header(): ReactElement {
   const [openNav, setOpenNav] = useState(false)
-  const { isLoggedIn } = useContext(userContext)
+  const { isLoggedIn, user } = useContext(userContext)
+  const { searchUsers } = useContext(showingUsersContext)
   const { notification } = useContext(messagesContext)
   const toast = useToast()
   useEffect(() => {
@@ -63,6 +67,47 @@ function Header(): ReactElement {
       })
     }
   }, [notification])
+  useEffect(() => {
+    if (user && user.x === 1000 && user.y === 1000) {
+      (async () => {
+        const location = navigator.geolocation
+        if (!location) {
+          console.log("There's no access to the geolocation.")
+          return
+        }
+        const position = (await new Promise(res => location.getCurrentPosition(val => res(val)))) as GeolocationPosition
+        if (!position?.coords?.latitude || !position?.coords?.longitude) return
+        const { latitude, longitude } = position.coords
+        const result = await client.mutate({
+          mutation: queries.editUser, variables: {
+            editUserVariables: {
+              id: user.id ?? null,
+              x: longitude,
+              y: latitude
+            }
+          }
+        })
+        if (result?.data?.editUser) {
+          toast({
+            title: "Bien hecho!",
+            description: "Tu ubicación fue actualizada.",
+            status: "success",
+            duration: 2000,
+            isClosable: true
+          })
+          searchUsers()
+        } else {
+          toast({
+            title: "Error",
+            description: "Tu ubicación no se pudo actualizar...",
+            status: "error",
+            duration: 2000,
+            isClosable: true
+          })
+        }
+      })()
+    }
+  }, [user])
   return (
     <Box bg="red.500" as="header">
       <Flex py="2" px="3" align="center">
